0833ff18552136e8f634b95a6578d44d277f74c9
Luan Farias <luan.farias_bvs@outlook.com>
2020-07-30 22:58:37 -0300

    Feed Editor is working and add a post

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -9,8 +9,8 @@
         <div class="row">
             <div class="column pr-5">
                 
-                <?= $render('feed-editor'); ?>
-                
+                <?= $render('feed-editor', ['user' => $loggedUser]); ?>
+
                 <?= $render('feed-item'); ?>
 
             </div>
