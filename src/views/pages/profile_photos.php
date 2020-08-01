c58fe2653dd4c02de5da10d361585a9e48d43347
Luan Farias <luan.farias_bvs@outlook.com>
2020-08-01 12:11:36 -0300

    The header of profile was divided in a partial

--- src/views/pages/profile_photos.php
+++ src/views/pages/profile_photos.php
@@ -5,74 +5,40 @@
 
     <section class="feed">
 
-            <div class="row">
-                <div class="box flex-1 border-top-flat">
-                    <div class="box-body">
-                        <div class="profile-cover" style="background-image: url('<?= $base; ?>/media/covers/<?= $user->cover; ?>');"></div>
-                        <div class="profile-info m-20 row">
-                            <div class="profile-info-avatar">
-                                <img src="<?= $base ?>/media/avatars/avatar.jpg" />
-                            </div>
-                            <div class="profile-info-name">
-                                <div class="profile-info-name-text"><?= $user->name; ?></div>
-                                <div class="profile-info-location"><?= $user->city ?? ''; ?></div>
-                            </div>
-                            <div class="profile-info-data row">
-                                <?php if($user->id !== $loggedUser->id): ?>
-                                <div class="profile-info-item m-width-20">
-                                    <a class="button" href="<?= $base; ?>/perfil/<?= $user->id; ?>/follow"><?= !$isFollowing ? 'Seguir' : 'Deixar de seguir'; ?></a>
-                                </div>
-                                <?php endif; ?>
-                                <div class="profile-info-item m-width-20">
-                                    <div class="profile-info-item-n"><?= count($user->followers); ?></div>
-                                    <div class="profile-info-item-s">Seguidores</div>
-                                </div>
-                                <div class="profile-info-item m-width-20">
-                                    <div class="profile-info-item-n"><?= count($user->following); ?></div>
-                                    <div class="profile-info-item-s">Seguindo</div>
-                                </div>
-                                <div class="profile-info-item m-width-20">
-                                    <div class="profile-info-item-n"><?= count($user->photos); ?></div>
-                                    <div class="profile-info-item-s">Fotos</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <?= $render('perfil-header', ['user' => $user, 'loggedUser' => $loggedUser, 'isFollowing' => $isFollowing]); ?>
             
-            <div class="row">
-               <div class="column">
-                    
-                    <div class="box">
-                        <div class="box-body">
+        <div class="row">
+            <div class="column">
+                
+                <div class="box">
+                    <div class="box-body">
 
-                            <div class="full-user-photos">
+                        <div class="full-user-photos">
 
-                                <?php if(count($user->photos) === 0): ?>
-                                    Esse usuário não possue fotos
-                                <?php endif; ?>
+                            <?php if(count($user->photos) === 0): ?>
+                                Esse usuário não possue fotos
+                            <?php endif; ?>
 
-                                <?php foreach ($user->photos as $photo): ?>
-                                <div class="user-photo-item">
-                                    <a href="#modal-<?= $photo->id; ?>" rel="modal:open">
-                                        <img src="<?= $base; ?>/media/uploads/<?= $photo->body; ?>" />
-                                    </a>
-                                    <div id="modal-<?= $photo->id; ?>" style="display:none">
+                            <?php foreach ($user->photos as $photo): ?>
+                            <div class="user-photo-item">
+                                <a href="#modal-<?= $photo->id; ?>" rel="modal:open">
                                     <img src="<?= $base; ?>/media/uploads/<?= $photo->body; ?>" />
-                                    </div>
-                                </div>   
-                                <?php endforeach; ?>
-
-                            </div>
+                                </a>
+                                <div id="modal-<?= $photo->id; ?>" style="display:none">
+                                <img src="<?= $base; ?>/media/uploads/<?= $photo->body; ?>" />
+                                </div>
+                            </div>   
+                            <?php endforeach; ?>
 
                         </div>
+
                     </div>
-
                 </div>
-            </div>
 
-        </section>
+            </div>
+        </div>
+
+    </section>
 
 </section>
 
