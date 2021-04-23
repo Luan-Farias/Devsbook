5d47f2e7a05431c58360d4994d3f6051690a8fd0
Luan Farias <luan.farias_bvs@outlook.com>
2021-04-23 19:29:09 -0300

    Added photos post upload and changed file permissions to linux

--- src/controllers/AjaxController.php
+++ src/controllers/AjaxController.php
@@ -51,4 +51,65 @@ class AjaxController extends Controller {
         exit;
     }
 
+    public function upload() {
+        $array = ['error' => ''];
+
+        if (isset($_FILES['photo']) && !empty($_FILES['photo']['tmp_name'])) {
+            $photo = $_FILES['photo'];
+
+            $maxWidth = 800;
+            $maxHeight = 800;
+
+            if (in_array($photo['type'], ['image/png', 'image/jpg', 'image/jpeg'])) {
+                list($originalWidth, $originalHeight) = getimagesize($photo['tmp_name']);
+
+                $ratio = $originalWidth / $originalHeight;
+
+                $newWidth = $maxWidth;
+                $newHeight = $maxHeight;
+                $maxRatio = $maxWidth / $maxHeight;
+
+                if ($maxRatio > $ratio) {
+                    $newWidth = $newHeight * $ratio;
+                } else {
+                    $newHeight = $newWidth / $ratio;
+                }
+
+                $finalImage = imagecreatetruecolor($newWidth, $newHeight);
+                switch($photo['type']) {
+                    case 'image/png':
+                        $image = imagecreatefrompng($photo['tmp_name']);
+                        break;
+                    case 'image/jpg':
+                    case 'image/jpeg':
+                        $image = imagecreatefromjpeg($photo['tmp_name']);
+                        break;
+                }
+
+                imagecopyresampled(
+                    $finalImage, $image, 
+                    0, 0, 0, 0, 
+                    $newWidth, $newHeight, $originalWidth, $originalHeight
+                );
+
+                $photoName = md5(time() . rand(0, 9999)) . '.jpg';
+                imagejpeg($finalImage, 'media/uploads/' . $photoName);
+
+                PostHandler::addPost(
+                    $this->loggedUser->id,
+                    'photo',
+                    $photoName
+                );
+            } else {
+                $array['error'] = 'Tipo de arquivo não suportado!';
+            }
+        } else {
+            $array['error'] = 'Nenhuma imagem enviada';
+        }
+
+        header("Content-Type: application/json");
+        echo json_encode($array);
+        exit;
+    }
+
 }
